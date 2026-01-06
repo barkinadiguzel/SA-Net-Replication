273d892c11b4d4c88ce8104a67343e5eff81f21a
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-01-06 11:01:58 +0300

    Create conv_layer.py

---
+++ src/layers/conv_layer.py
@@ -0,0 +1,7 @@
+import torch.nn as nn
+
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+
+def conv1x1(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
